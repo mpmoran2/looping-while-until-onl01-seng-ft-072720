3ec709cc9867c6d1f22f0bc02d0099b05be4a9cf
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 22:58:24 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,21 @@
+
 def using_while
+<<<<<<< HEAD
   levitation_force = 6
     while levitation_force < 10
     puts 'Wingardium Leviosa'
   levitation_force += 1
   end
 end
+=======
+	loop do
+	  while levitation_force < 10
+	  puts "Wingardium Leviosa"
+	  counter +=1
+    end
+  end
+end  
+>>>>>>> a29476d119b5e487948025f238faa8b3dff5739a
 
 
 
